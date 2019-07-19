540d0ff874770d89d7e1bfe188af32fe26ee5669
ines uwase <uwaseines10@gmail.com>
2019-07-19 09:03:55 +0200

    i made changes on the links

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var day =  prompt("What is ur date of birth?");
+var month =  prompt("What is ur month of birth?");
+var year =  prompt("What is ur year of birth?");
+var gender =  prompt("What is ur gender?");
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" , "Kof", "Kwame"]
+var c
