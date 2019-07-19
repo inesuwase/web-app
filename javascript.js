6f19403cb04a16f32010956c6a1f74a149331e0e
ines uwase <uwaseines10@gmail.com>
2019-07-19 11:55:07 +0200

    i have created a function

--- javascript.js
+++ javascript.js
@@ -4,4 +4,34 @@ var year =  prompt("What is ur year of birth?");
 var gender =  prompt("What is ur gender?");
 var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" , "Kof", "Kwame"]
-var c
+function birthdate() {
+ var d = parseFloat(document.getElementById('day').value);
+ var m = parseFloat(document.getElementById('month').value);
+ var y = parseFloat(document.getElementById('year').value);
+ var c = (y - 1) / 100 + 1;
+ var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
+ console.log(dayoftheweek);
+ console.log(dayoftheweek);
+ document.getElementById('result').innerHTML = Math.round(dayoftheweek);
+var choice = document.getElementById('select').value;
+
+ if (choice === "male") {
+     console.log(gabo[Math.round(dayoftheweek)]);
+
+     document.getElementById('akan').innerHTML = gabo[Math.round(dayoftheweek)];
+
+
+
+
+
+ } else if (choice === "female") {
+
+
+
+
+
+     console.log(gore[Math.round(dayoftheweek)]);
+     document.getElementById('akan').innerHTML = gore[Math.round(dayoftheweek)];
+ } else {
+     console.log("invalid outuput");
+ }
