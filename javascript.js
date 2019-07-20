15ab98e75516cead239e5d88dfef1c1a033a2dca
ines uwase <uwaseines10@gmail.com>
2019-07-20 13:49:51 +0200

    i have used arrays

--- javascript.js
+++ javascript.js
@@ -1,36 +1,33 @@
-var day =  prompt("What is ur date of birth?");
-var month =  prompt("What is ur month of birth?");
-var year =  prompt("What is ur year of birth?");
-var gender =  prompt("What is ur gender?");
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" , "Kof", "Kwame"]
 function birthdate() {
  var d = parseFloat(document.getElementById('day').value);
  var m = parseFloat(document.getElementById('month').value);
  var y = parseFloat(document.getElementById('year').value);
  var c = (y - 1) / 100 + 1;
+ var day=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
  var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
  console.log(dayoftheweek);
- console.log(dayoftheweek);
  document.getElementById('result').innerHTML = Math.round(dayoftheweek);
+ var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" , "Kof", "Kwame"]
+var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 var choice = document.getElementById('select').value;
+if (d>31 || d>1){
+    alert("This day doesn't exist");
+}
 
- if (choice === "male") {
-     console.log(gabo[Math.round(dayoftheweek)]);
-
-     document.getElementById('akan').innerHTML = gabo[Math.round(dayoftheweek)];
-
-
-
-
- } else if (choice === "female") {
-
-
-
-
-
-     console.log(gore[Math.round(dayoftheweek)]);
-     document.getElementById('akan').innerHTML = gore[Math.round(dayoftheweek)];
+if (m>12 || m<1){
+    alert("This month doesn't exist");
+}
+if (y>2019 || y<1){
+    alert("this year doesn't exist");
+}
+ if (gender === "male") {
+     console.log(male[Math.round(dayoftheweek)]);
+     document.getElementById('AKAN').innerHTML = male[Math.round(dayoftheweek)];
+ } 
+ else if (gender === "female") {
+    console.log(female[Math.round(dayoftheweek)]);
+     document.getElementById('AKAN').innerHTML = female[Math.round(dayoftheweek)];
  } else {
      console.log("invalid outuput");
  }
+ 
