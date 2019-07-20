8934008b07f6308d0a290143342abf5bdaf2de4b
ines uwase <uwaseines10@gmail.com>
2019-07-20 15:21:44 +0200

    i made spaces

--- javascript.js
+++ javascript.js
@@ -1,14 +1,14 @@
-function birthdate() {
+function birthdates() {
  var d = parseFloat(document.getElementById('day').value);
  var m = parseFloat(document.getElementById('month').value);
  var y = parseFloat(document.getElementById('year').value);
  var c = (y - 1) / 100 + 1;
- var day=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
  var dayoftheweek = (c / 4 - 2 * c - 1 + 5 * y / 4 + 26 * (m + 1) / 10 + d) % 7;
+ var day=["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
  console.log(dayoftheweek);
- document.getElementById('result').innerHTML = Math.round(dayoftheweek);
- var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" , "Kof", "Kwame"]
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+ document.getElementById('result').innerHTML = day[Math.round(dayoftheweek)];
+ var male = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw" , "Kof", "Kwame"];
+var female = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 var choice = document.getElementById('select').value;
 if (d>31 || d>1){
     alert("This day doesn't exist");
@@ -19,14 +19,15 @@ if (m>12 || m<1){
 if (y>2019 || y<1){
     alert("this year doesn't exist");
 }
- if (gender === "male") {
+ if (choice === "male") {
      console.log(male[Math.round(dayoftheweek)]);
-     document.getElementById('AKAN').innerHTML = male[Math.round(dayoftheweek)];
+     document.getElementById('Akan').innerHTML = male[Math.round(dayoftheweek)];
  } 
- else if (gender === "female") {
+ else if (choice === "female") {
     console.log(female[Math.round(dayoftheweek)]);
-     document.getElementById('AKAN').innerHTML = female[Math.round(dayoftheweek)];
- } else {
+     document.getElementById('Akan').innerHTML = female[Math.round(dayoftheweek)];
+ } 
+ else {
      console.log("invalid outuput");
  }
  
